bb2039fdf12ff91287c325426f61b44033ca207b
Oliwer Kiszka <oliverkiszka9@gmail.com>
2024-04-10 19:13:18 +0200

    added WaterContainer model

---
+++ src/main/java/pl/kurs/watercontainers/models/WaterContainer.java
@@ -0,0 +1,26 @@
+package pl.kurs.watercontainers.models;
+
+public class WaterContainer {
+
+    private String name;
+    private double maxCapacity;
+    private double waterLevel;
+
+    public WaterContainer(String name, double maxCapacity, double waterLevel) {
+        this.name = name;
+        this.maxCapacity = maxCapacity;
+        this.waterLevel = waterLevel;
+    }
+
+
+
+
+    @Override
+    public String toString() {
+        return "WaterContainer{" +
+                "name='" + name + '\'' +
+                ", maxCapacity=" + maxCapacity +
+                ", waterLevel=" + waterLevel +
+                '}';
+    }
+}
