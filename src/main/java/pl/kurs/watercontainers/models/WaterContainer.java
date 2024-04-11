537556344aa0486fa5f2a30ffaeef5fe837f82c9
Oliwer Kiszka <oliverkiszka9@gmail.com>
2024-04-11 22:16:48 +0200

    added addWater() method

--- src/main/java/pl/kurs/watercontainers/models/WaterContainer.java
+++ src/main/java/pl/kurs/watercontainers/models/WaterContainer.java
@@ -1,6 +1,7 @@
 package pl.kurs.watercontainers.models;
 
 import pl.kurs.watercontainers.exceptions.InvalidCapacityException;
+import pl.kurs.watercontainers.exceptions.InvalidWaterAmountException;
 import pl.kurs.watercontainers.exceptions.InvalidWaterLevelException;
 
 import java.io.Serializable;
@@ -17,12 +18,23 @@ public class WaterContainer implements Serializable {
         this.maxCapacity = maxCapacity;
         this.waterLevel = waterLevel;
     }
-    public static WaterContainer create(String name, double maxCapacity, double waterLevel){
+
+    public static WaterContainer create(String name, double maxCapacity, double waterLevel) {
         if (maxCapacity <= 0)
             throw new InvalidCapacityException("Max capacity must be more than zero");
         if (waterLevel < 0 || waterLevel > maxCapacity)
             throw new InvalidWaterLevelException("Invalid water level value");
         return new WaterContainer(name, maxCapacity, waterLevel);
+    }
+
+    public void addWater(double value) {
+        if (value <= 0) {
+            throw new InvalidWaterAmountException("Water value should be more than 0");
+        }
+        if (waterLevel + value > maxCapacity) {
+            throw new InvalidWaterAmountException("Too much water to add!");
+        }
+        waterLevel += value;
     }
 
 
